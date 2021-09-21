3688adeef049e4f871645ae5396d188d9a6f681a
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2021-09-21 19:10:53 -0300

    fix: remove mongoose-pagination and improve mongo connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect('mongodb://mongo:decks@mongo-decks:27017/decks', {
+mongoose.connect("mongodb://mongo:123456@localhost:27017/decks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,21 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 
-import bodyParser from 'body-parser';
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import express from "express";
 
-import router from './router'
+import router from "./router";
 
-import './database';
-import './database/schemas/User';
-import './database/schemas/Category';
-import './database/schemas/Card';
-import './database/schemas/Question';
-
-import schema from './schemas';
+import "./database";
+import "./database/schemas/User";
+import "./database/schemas/Category";
+import "./database/schemas/Card";
+import "./database/schemas/Question";
 
 const server = express();
 
-server.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(express.static('dist'));
+server.use(express.json());
 server.use(router);
 
-const apolloServer = new ApolloServer({
-  schema,
-  context: ({ req }) => {
-    const context = {
-      req,
-      token: req?.headers?.authorization
-    }
-
-    return context;
-  }
-});
-apolloServer.applyMiddleware({ app: server, path: '/graphql' });
-
 const port = process.env.PORT || 4000;
-server.listen({port}, () => console.log(`Server is running on port ${port}`))
+server.listen({ port }, () => console.log(`Server is running on port ${port}`));
 
 export default server;
